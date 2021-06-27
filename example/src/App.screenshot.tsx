ee61348c5e1356e0956eeecbf9bffc017a354600
François Wouts <f@zenc.io>
2021-06-27 22:24:39 +1000

    Delete screenshot

--- example/src/App.screenshot.tsx
+++ example/src/App.screenshot.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import App from "./App";
 
-export const Screenshot1 = () => <div>Hello, World!</div>;
-
 export const Screenshot2 = () => <App />;
 
 export const Screenshot3 = () => {
