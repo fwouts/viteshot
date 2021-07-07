672aaf78045f3f8b1b7fc1941a57382984fa2a7a
François Wouts <f@zenc.io>
2021-07-07 10:41:52 +1000

    Remove error screenshots (#18)
    Each error stack is slightly different, so they make tests flaky.

--- svelte-example/src/lib/Error.screenshot.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export function beforeScreenshot(element: HTMLElement) {
-    throw new Error(`An error should be shown`);
-  }
-</script>
-
-<div>This should not be shown</div>
