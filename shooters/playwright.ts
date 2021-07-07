14a217866e8eeaece7ad2b39e9bf47bfd1310d4c
François Wouts <f@zenc.io>
2021-07-07 10:26:39 +1000

    WIP

--- shooters/playwright.ts
+++ shooters/playwright.ts
@@ -4,12 +4,18 @@ import { BrowserConfig } from "../src/config";
 
 export default (
   browserType: playwright.BrowserType<{}>,
-  contextOptions: playwright.BrowserContextOptions = {}
+  options: {
+    context?: playwright.BrowserContextOptions;
+    output?: {
+      prefixPath?: string;
+      suffixPath?: string;
+    };
+  } = {}
 ): BrowserConfig<playwright.Page> =>
   ({
     launchBrowser: async () => {
       const browser = await browserType.launch();
-      const context = await browser.newContext(contextOptions);
+      const context = await browser.newContext(options.context);
       return {
         newPage: async () => {
           const page = await context.newPage();
@@ -24,10 +30,17 @@ export default (
       };
     },
     captureScreenshot: async (page: playwright.Page, name: string) => {
+      let prefixPath = options.output?.prefixPath;
+      if (prefixPath === undefined) {
+        prefixPath = "__screenshots__";
+      }
+      const suffixPath = options.output?.suffixPath || "";
+      const dirPath = path.dirname(name);
+      const baseName = path.basename(name);
       console.log(`Capturing: ${name}`);
       await page.screenshot({
         fullPage: true,
-        path: path.join("__screenshots__", `${name}.png`),
+        path: path.join(prefixPath, dirPath, suffixPath, `${baseName}.png`),
       });
     },
   } as const);
