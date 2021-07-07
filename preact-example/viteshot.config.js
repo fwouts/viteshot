aa8d4e7a2764000e80f927ec621357ca62ff336b
François Wouts <f@zenc.io>
2021-07-07 10:58:54 +1000

    Use custom output

--- preact-example/viteshot.config.js
+++ preact-example/viteshot.config.js
@@ -3,10 +3,13 @@ const playwright = require("playwright");
 
 module.exports = {
   framework: "preact",
-  browser: playwrightShooter(
-    playwright.chromium,
-    playwright.devices["Pixel 2"]
-  ),
+  browser: playwrightShooter(playwright.chromium, {
+    context: playwright.devices["Pixel 2"],
+    output: {
+      prefixPath: "",
+      suffixPath: "__screenshots__",
+    },
+  }),
   wrapper: {
     path: "__reactpreview__/Wrapper",
     componentName: "Wrapper",
