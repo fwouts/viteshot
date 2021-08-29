0396ce97fd2a95a1e637c56543862679b631fcd2
François Wouts <f@zenc.io>
2021-08-30 08:26:30 +1000

    Include vite-plugin-solid explicitly in Solid config

--- src/frameworks/solid.ts
+++ src/frameworks/solid.ts
@@ -1,9 +1,11 @@
+// @ts-ignore This package is not installed by default.
+import solidPlugin from "vite-plugin-solid";
 import { FrameworkConfiguration } from "./config";
 
 export function solidConfiguration(): FrameworkConfiguration {
   return {
     packages: ["solid-js"],
     defaultImports: false,
-    plugins: [],
+    plugins: [solidPlugin()],
   };
 }
