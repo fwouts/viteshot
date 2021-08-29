3cba4bc86709e25acf8d16886d03b2cf3c7fb735
François Wouts <f@zenc.io>
2021-08-30 08:42:48 +1000

    Attempt to fix import

--- src/frameworks/solid.ts
+++ src/frameworks/solid.ts
@@ -1,8 +1,8 @@
-// @ts-ignore This package is not installed by default.
-import solidPlugin from "vite-plugin-solid";
 import { FrameworkConfiguration } from "./config";
 
 export function solidConfiguration(): FrameworkConfiguration {
+  // Note: This package is not installed by default.
+  const solidPlugin = require("vite-plugin-solid");
   return {
     packages: ["solid-js"],
     defaultImports: false,
